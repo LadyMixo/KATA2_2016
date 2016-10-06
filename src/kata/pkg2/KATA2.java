71772335788d1f202ad5586b5a02923e7fcfd75e
usuario <usuario@10.22.143.52>
2016-10-06 11:23:17 +0100

    Kata 2 --Versión 3- [Clase Histogram]
    En esta versión creamos un fichero nuevo para la clase Histogran donde inicializamos el histograma y lo recorremos quedándo en el Main simplemente crear el objeto Histogram y darle valor y  contar las apariciones de las claves y mostrarlas por pantalla

--- src/kata/pkg2/KATA2.java
+++ src/kata/pkg2/KATA2.java
@@ -7,17 +7,13 @@ public class KATA2 {
 
     public static void main(String[] args) {
         int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-     Map<Integer, Integer>  histogram = new HashMap<>();   
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
      
-     // si el histograma  y comprobamos si  existe la clave i, si existe aumentamos su contador, sino ponemos 1
-        for (int i : data) {
-            histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1 : 1);
-        }
-            
-        
+       
         // recorremos el mapa por sus claves y mostramos el numero de repeticiones
-        for (int key : histogram.keySet()) { //fore+tab // KeySet() -> conjunto de claves del mapa
-            System.out.println(key + " --> " + histogram.get(key));
+        for (int key : histogr.keySet()) { //fore+tab // KeySet() -> conjunto de claves del mapa
+            System.out.println(key + " --> " + histogr.get(key));
    
         }
     }
