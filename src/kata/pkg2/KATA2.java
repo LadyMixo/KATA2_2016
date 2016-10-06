7d534d1c8a2be81831a9b42cc0288a32a9854520
usuario <usuario@10.22.143.52>
2016-10-06 11:33:00 +0100

    Kata 2 --Versión - [clase Histogram Generica]
    En esta versión cambiamos el tipo de Histogram a genérico y modificamos en el main el tipo de entrada de Int a Integer y a String

--- src/kata/pkg2/KATA2.java
+++ src/kata/pkg2/KATA2.java
@@ -6,13 +6,22 @@ import java.util.Map;
 public class KATA2 {
 
     public static void main(String[] args) {
-        int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        
+        // como hablamos de tipo generíco tenemos que cambiar Int por integer
+       // Integer[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        
+        String[] data={"Ana", "Ana", "Ana", "Luis", "Luis", "Rosa"}; 
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        
+       // Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
      
-       
         // recorremos el mapa por sus claves y mostramos el numero de repeticiones
-        for (int key : histogr.keySet()) { //fore+tab // KeySet() -> conjunto de claves del mapa
+        //for (int key : histogr.keySet()) { //fore+tab // KeySet() -> conjunto de claves del mapa
+        
+        /* Para recorrer una Strig tnemos que cambiar el tipo de clave que le vamos a pasar
+         String en este caso */
+        for (String key : histogr.keySet()) { //fore+tab // KeySet() -> conjunto de claves del mapa
             System.out.println(key + " --> " + histogr.get(key));
    
         }
